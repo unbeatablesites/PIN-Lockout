f658ef476931c97ab46b01c171b10a40dde38b77
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-21 20:17:59 -0400

    Finish app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        
+        Scanner keyboard = new Scanner(System.in);
+        int pin = 12345;
+        int tries = 0;
+
+        System.out.println("WELCOME TO THE BANK OF Frank.");
+        System.out.print("ENTER YOUR PIN: ");
+        int entry = keyboard.nextInt();
+        tries++;
+
+        while ( entry != pin && tries < 3 )
+        {
+            System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+            System.out.print("ENTER YOUR PIN: ");
+            entry = keyboard.nextInt();
+            tries++;
+        }
+
+        if ( entry == pin )
+            System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+        else if ( tries >= 3 )
+            System.out.println("\nYOU HAVE RUN OUT OF TRIES. ACCOUNT LOCKED.");
     }
 }
